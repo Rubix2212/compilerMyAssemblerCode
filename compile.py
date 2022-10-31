bd0eefc078b82a882ffb14de4492fa806fdd6094
Rubix2212 <diegothiry26@gmail.com>
2022-10-31 13:29:59 -0300

    first commit

--- compile.py
+++ compile.py
@@ -1,14 +1,53 @@
+from io import TextIOWrapper
+from sys import argv
+from turtle import st
+import numpy as np
 from src.dictionaries import *
 from os import system
+	
 
-def compile(file, compiled_file):
+def handleAddress(pc_at: list, pc_tag: list, to_search: str):
+	try:
+		index = pc_tag.index(to_search)
+	except:
+		print(f"La etiqueta {to_search} no existe")
+		system("pause")
+		exit(0)
+
+	return pc_at.__getitem__(index)
+
+def compile(file: TextIOWrapper, compiled_file: TextIOWrapper):
 	counter = 0
+	i = 0
+	pc_at = []
+	pc_tag = []
+	file2 = file
 	while True:
-		system("cls")
+		linea2 = file2.readline()
+		if not linea2:
+			break
+		if linea2[0][0] == "@":
+			pc = hex(counter).replace('0x','').zfill(3)
+			tag = linea2.replace('@', '').replace(':', '').replace('\n', '')
+			pc_at.append(pc)
+			pc_tag.append(tag)
+			i += 1
+			continue
+	
+		counter += 3		
+	counter = 0
+	jj = 1
+	file = open(argv[1], "r")
+	while jj:
 		linea = file.readline()
 		if not linea:
 			break
-		counter += 3
+		if linea[0][0] == "@":
+			PC = hex(counter).replace('0x','').zfill(3)
+			compiled_file.write(f"[0x{PC}] {linea}")
+			continue
+
+		
 		linea = str(linea).split()
 		op = opcodes.get(linea[0])
 		opcode = bin(op).replace('0b', '').zfill(4)
@@ -19,16 +58,20 @@ def compile(file, compiled_file):
 			funct = bin(functs.get(linea[0])).replace('0b', '').zfill(5)
 			inst_hex = hex(int(opcode + r1 + r2 + rd + funct, 2)).replace('0x', '').zfill(6)
 
-			compiled_file.write(f"[{hex(counter)}] 0x" + inst_hex + " # " + str(linea) + "\n")
+			PC = hex(counter).replace('0x','').zfill(3)
+			compiled_file.write(f"[0x{PC}] 0x" + inst_hex + "    			# " + str(linea) + "\n")
 		elif op == 3 or op == 11:
-			compiled_file.write(f"[{hex(counter)}] {hex(int(opcode, 2))}00000" + " # " + str(linea) + "\n")
+			PC = hex(counter).replace('0x','').zfill(3)
+			compiled_file.write(f"[0x{PC}] {hex(int(opcode, 2))}00000" + "    			# " + str(linea) + "\n")
 		elif op >= 6 and op <= 9:
 			r1 = bin(regs.get(linea[1].replace(',', ''))).replace('0b', '').zfill(5)
 			r2 = bin(regs.get(linea[2].replace(',', ''))).replace('0b', '').zfill(5)
-			address = bin(int(linea[3].replace(',', ''), 16)).replace('0b', '').zfill(10)
+			address = handleAddress(pc_at, pc_tag, linea[3].replace(',', '').lower())
+			address = bin(int(address, 16)).replace('0b', '').zfill(10)
 
 			inst_hex = hex(int(opcode + r1 + r2 + address, 2)).replace('0x', '').zfill(6)
-			compiled_file.write(f"[{hex(counter)}] 0x" + inst_hex + " # " + str(linea) + "\n")
+			PC = hex(counter).replace('0x','').zfill(3)
+			compiled_file.write(f"[0x{PC}] 0x" + inst_hex + "    			# " + str(linea) + "\n")
 
 		elif op == 4:
 			rd = bin(regs.get(linea[1].replace(',', ''))).replace('0b', '').zfill(5)
@@ -36,24 +79,25 @@ def compile(file, compiled_file):
 			const = bin(int(linea[3].replace(',', ''), 10)).replace('0b', '').zfill(10)
 
 			inst_hex = hex(int(opcode + r1 + rd + const, 2)).replace('0x', '').zfill(6)
-			compiled_file.write(f"[{hex(counter)}] 0x" + inst_hex + " # " + str(linea) + "\n")
+			PC = hex(counter).replace('0x','').zfill(3)
+			compiled_file.write(f"[0x{PC}] 0x" + inst_hex + "    			# " + str(linea) + "\n")
 		elif op == 5 or op == 10:
-			address = bin(int(linea[1].replace(',', ''), 16)).replace('0b', '').zfill(10)
-			inst_hex = hex(int(address, 2)).replace('0x', '').zfill(6)
-			compiled_file.write(f"[{hex(counter)}] 0x" + inst_hex + " # " + str(linea) + "\n")
+			address = handleAddress(pc_at, pc_tag, linea[1].replace(',', '').lower())
+			address = bin(int(address, 16)).replace('0b', '').zfill(10)
+			inst_hex = hex(int(opcode + "0000000000" + address, 2)).replace('0x', '')
+			compiled_file.write(f"[0x{PC}] 0x" + inst_hex + "    			# " + str(linea) + "\n")
+		
+		counter += 3 
 	print("Compile successfully!")
-	system("pause")
-	system("cls")
 
 def main():
-	name = input("Ingrese archivo: ")
 	try:
-		file = open(name, 'r')
+		file = open(argv[1], 'r')
 	except:
 		print("Error, archivo no existente!")
 		exit(0)
 
-	compiled_file = open(name.replace(".asm", '').replace(".s", '') + ".bin", 'w')
+	compiled_file = open(argv[1].replace(".asm", '').replace(".s", '') + ".bin", 'w')
 	compile(file, compiled_file)
 
 	file.close()
--- programas/fib.s
+++ programas/fib.s
@@ -1,12 +1,16 @@
-ADDC R1, R0, 10  
-ADDC R3, R0, 1	 
-JAL 0xc					 
-EXIT						 
-ADDC R5, R5, 1   
-JEQ R5, R1, 0x21 
-ADD R4, R2, R3   
-ADDC R2, R3, 0   
-ADDC R3, R4, 0   
-ADDC R5, R5, 1   
-JMP 0xf					 
-RET							 
+@main:
+ADDC R1, R0, 10
+ADDC R3, R0, 1
+JAL startloop
+EXIT
+@startloop:
+ADDC R5, R5, 1
+@loop:
+JEQ R5, R1, return
+ADD R4, R2, R3
+ADDC R2, R3, 0
+ADDC R3, R4, 0
+ADDC R5, R5, 1
+JMP loop
+@return:
+RET
